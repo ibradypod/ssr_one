b1b61e6ebe3a063bfdad3684fb3f7ca9bbe4944b
ibradypod <dznnnn@163.com>
2018-09-17 23:14:01 +0800

    Create ssr.sh

---
+++ ssr.sh
@@ -0,0 +1,6 @@
+# bin/sh
+wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh
+chmod +x shadowsocksR.sh
+./shadowsocksR.sh 2>&1 | tee shadowsocksR.log
+
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
